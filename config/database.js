957a96b9920ce6ade0420de3aee470bba3c266ec
tran minh anh <anh.tran@beetech.com.vn>
2023-06-19 12:04:33 +0700

    push database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        autoIndex: false
-    }).then(con => {
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+  mongoose
+    .connect(process.env.DB_LOCAL_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      autoIndex: false
     })
-}
+    .then(() => {
+      console.log(`MongoDB Database connected with HOST: ${mongoose.connection.host}`);
+    })
+    .catch(error => {
+      console.error(`MongoDB Connection Error: ${error}`);
+    });
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
