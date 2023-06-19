ddc60d5e8a843f2e6d667b3b833c1b0c4d1694d8
tran minh anh <anh.tran@beetech.com.vn>
2023-06-19 18:27:56 +0700

    push

--- app.js
+++ app.js
@@ -23,15 +23,6 @@ app.use(cookieParser())
 app.use(fileUpload());
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-  })
 
 
 // Import all routes
